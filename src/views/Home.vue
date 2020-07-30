b0df55bc60c3b4690edab40454d54ae09d77ace8
akhmadkha <pt.janishlintasdata@gmail.com>
2020-07-30 14:24:10 +0700

    Fix Centered Object

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex px-10 mt-10">
-    <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between px-10 lg:w-4/6">
+    <div class="hero container-inner mx-auto flex flex-col items-center sm:flex-row justify-between px-10 lg:w-4/6">
       <div class="text-4xl font-bold sm:w-3/5 text-center sm:text-left">
         <div class="leading-tight tracking-tighter">Ahmad Shobirin</div>
         <div class="text-red-400 text-2xl">Web Developer</div>
