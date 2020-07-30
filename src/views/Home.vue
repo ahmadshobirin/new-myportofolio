7d55195743fb980741a99ec326a9e217a41e88d8
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2020-07-30 14:01:57 +0700

    set fit home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex h-screen px-10 py-10">
-    <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16 px-10 lg:w-4/6">
+  <div class="flex px-10 mt-10">
+    <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between px-10 lg:w-4/6">
       <div class="text-4xl font-bold sm:w-3/5 text-center sm:text-left">
         <div class="leading-tight tracking-tighter">Ahmad Shobirin</div>
         <div class="text-red-400 text-2xl">Web Developer</div>
